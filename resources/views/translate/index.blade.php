7be33c2eb3d9983799b4c0f5aa299033a31b0532
Matt McGrath <mcm223@g.harvard.edu>
2018-05-04 23:22:32 -0700

    Revise intro text

--- resources/views/translate/index.blade.php
+++ resources/views/translate/index.blade.php
@@ -13,7 +13,8 @@
     <h2>Babel: Translation Service</h2>
     <div id='intro'>
         <p>Use the form below to translate text into the language of your choosing. You can either specify a
-            starting language, or let the system auto-detect what language you enter. You can also check out other
+            starting language, or let the system auto-detect what language you enter. Supported language combinations
+            are English to any other language, or any language into English. You can also check out other
             translations that have been entered on the <a href='/translations'>translations page</a>.</p>
     </div>
     @include('modules.form')
